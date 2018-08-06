f2bb6570edb817cf31b1bfd0bf90463e9943eb92
Christopher <cmmccoy1996@gmail.com>
2018-08-06 09:13:42 -0700

    The user can make a post request by clicking on the submitt button.

--- Node.js/movie_search_app/app.js
+++ Node.js/movie_search_app/app.js
@@ -2,11 +2,24 @@ var http=require('http');
 var express=require('express');
 var request=require('request');
 const app=express();
+app.use(express.static(__dirname + '/public'));
 
 var defaultTitle="Alien";
 
 
-app.use(express.static(__dirname + '/public'));
+
+app.post("/searchMovie",function (req,res) {
+    console.log("search!!!");
+    /*request("http://www.omdbapi.com/?s=star&apikey=thewdb",function (error,res,body) {
+    if(!error && res.statusCode==200){
+        var data=JSON.parse(body);
+        console.log(data["query"]["results"]["channel"]["location"]);
+
+    }
+    res.render('home.ejs',{});
+});*/
+});
+
 
 app.get('/',function (req,res) {
     res.render('home.ejs',{results:defaultTitle});
@@ -15,4 +28,4 @@ app.get('/',function (req,res) {
 app.listen(2000,function() {
     console.log("Listening on Port:"+2000);
 });
-//slk
+//slksfsd
