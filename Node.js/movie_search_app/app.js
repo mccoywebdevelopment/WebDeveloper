8cddc9761efb2b6428fcf5466cc8501bbc3e550f
Christopher <cmmccoy1996@gmail.com>
2018-08-06 09:51:56 -0700

    The user can look up a movie and display results in the terminal.

--- Node.js/movie_search_app/app.js
+++ Node.js/movie_search_app/app.js
@@ -1,8 +1,14 @@
 var http=require('http');
 var express=require('express');
 var request=require('request');
+var bodyParser=require('body-parser');
 const app=express();
+app.use(bodyParser.urlencoded({extended:true}));
 app.use(express.static(__dirname + '/public'));
+
+app.listen(2000,function() {
+    console.log("Listening on Port:"+2000);
+});
 
 var defaultTitle="Alien";
 var userSearch=defaultTitle;
@@ -10,6 +16,9 @@ var userSearch=defaultTitle;
 
 
 app.post("/searchMovie",function (req,res) {
+    var userSearch=req.body.user_title;
+
+    console.log(userSearch);
     console.log("http://www.omdbapi.com/?s="+userSearch+"&apikey=thewdb");
     request("http://www.omdbapi.com/?s="+userSearch+"&apikey=thewdb",function (error,res,body) {
     if(!error && res.statusCode==200){
@@ -25,7 +34,3 @@ app.get('/',function (req,res) {
     res.render('home.ejs',{results:defaultTitle});
 });
 
-app.listen(2000,function() {
-    console.log("Listening on Port:"+2000);
-});
-//slksfsd
