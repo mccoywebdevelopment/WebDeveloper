1eed6b4162bbef57b099be4c03377bbd360259fd
Christopher <cmmccoy1996@gmail.com>
2018-08-10 12:08:04 -0700

    Created an array of objects that will serve as our temp database until I implement the mogoDB.

--- Final Project/app.js
+++ Final Project/app.js
@@ -1,12 +1,21 @@
 var express=require("express");
 var app=express();
 
+//Variables======================================
+var campSites=[
+    {title:"Camp Pike",image:"https://cdn1.freshoffthegrid.com/wp-content/uploads/guide-to-free-campsites-in-the-us-and-canada.jpg"},
+    {title:"Camp Yolo",image:"https://www.weekendnotes.com/im/006/03/best-free-camping-in-south-australiabest-free-camp.jpg"},
+    {title:"Camp Steph",image:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRXAjkhsZYvkyu0PWBAdSxQm_8SFeieS6WljGqtHqlW-_UlKTLg9A"},
+    {title:"Camp Doe",image:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQww-oC_fV0vNa-cHvmImgHAtBiPYuXxU5ozmGQJM9q1TUQlZBnCA"}];
+//===============================================
+
+
 app.get("/",function (req,res) {
     res.send("Home page");
 });
 
 app.get("/landing",function (req,res) {
-    res.render("landing.ejs");
+    res.render("landing.ejs",{campSites:campSites});
 });
 
 app.listen(2000,function() {
