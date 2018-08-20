11bcb329485db83f85f663344ccacf9b66406e91
Christopher <cmmccoy1996@gmail.com>
2018-08-20 08:21:58 -0700

    Still need to check if img exists.

--- Final Project/app.js
+++ Final Project/app.js
@@ -1,6 +1,9 @@
 var express=require("express");
 var app=express();
 var bodyParser=require('body-parser');
+var checkImage = require('image-check');
+var fs=require("fs");
+
 app.use(bodyParser.urlencoded({extended:true}));
 
 //Variables======================================
@@ -12,6 +15,9 @@ var campSites=[
     {title:"Camp Yolo",image:"https://images.pexels.com/photos/176381/pexels-photo-176381.jpeg?auto=compress&cs=tinysrgb&h=350"},
     {title:"Camp Steph",image:"https://images.pexels.com/photos/14287/pexels-photo-14287.jpeg?auto=compress&cs=tinysrgb&h=350"},
     {title:"Camp Doe",image:"https://images.pexels.com/photos/111362/pexels-photo-111362.jpeg?auto=compress&cs=tinysrgb&h=350"}];
+
+var defaultImg="http://www.piniswiss.com/wp-content/uploads/2013/05/image-not-found-4a963b95bf081c3ea02923dceaeb3f8085e1" +
+    "a654fc54840aac61a57a60903fef-300x199.png";
 //===============================================
 
 
@@ -20,7 +26,7 @@ app.get("/",function (req,res) {
 });
 
 app.get("/campSite",function (req,res) {
-    res.render("campSite.ejs",{campSites:campSites});
+    res.render("campSite.ejs",{campSites:campSites,defaultImg:defaultImg});
 });
 
 app.get("/new",function (req,res) {
@@ -30,6 +36,16 @@ app.get("/new",function (req,res) {
 app.post("/campSite",function (req,res) {
     var newCampSite=req.body.newCampSite;
     var newCampSiteURL=req.body.newCampSiteURL;
+
+    checkImage(newCampSiteURL).then((data) => {
+        // access image attributes
+    console.log("True");
+    }).catch((err) => {
+        // handle error
+        console.log("False");
+        console.log(newCampSiteURL);
+    });
+
     campSites.push({title:newCampSite,image:newCampSiteURL});
     res.redirect("/campSite");
 });
